0dbe0a232d10dcdc9c4c02c7322bfcd9cb5a4126
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-04 20:02:05 +0700

    asdhfa

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server started'));
-app.get('/', (req, res) => res.render('home'));
+
+app.get('/', (req, res) => res.render('home', { mang : arrTin }));
+
+class Tin {
+    constructor(title, desc, idVideo, image) {
+
+    }
+}
+
+const arrTin = [
+    new Tin(), new Tin(), ...
+];
