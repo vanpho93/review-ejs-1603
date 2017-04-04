5a87d3ea09b934677148f11687f54e9f7bac0851
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-04 20:10:33 +0700

    create an array of news

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # review-ejs-1603
 
 Clone it: <hr/>https://github.com/vanpho93/reviewEJS-Multer-2311.git
+
+<hr />
+Bai 7 -> Bai 12
+https://www.youtube.com/watch?v=YjVp1Vewcew&list=PLzrVYRai0riRcHcnBzMSqLIVodfHo9YGU
--- index.js
+++ index.js
@@ -12,10 +12,14 @@ app.get('/', (req, res) => res.render('home', { mang : arrTin }));
 
 class Tin {
     constructor(title, desc, idVideo, image) {
-
+        this.title = title;
+        this.desc = desc;
+        this.idVideo = idVideo;
+        this.image= image;
     }
 }
 
 const arrTin = [
-    new Tin(), new Tin(), ...
+    new Tin('NodeJS Khoa Pham', 'NodeJS duoc phat trien nam 2009 boi Dahl', 203803551,'1.jpg' ), 
+    new Tin('ReactJS Khoa Pham', 'ReactJS la 1 thu vien javascript de xay dung UI', 208130024,'2.png')
 ];
